788ec841d01dc39ba6a04c1f0f51cf6db6ece01e
Alex23925 <alexhernandez102598@gmail.com>
2020-09-07 11:20:14 -0700

    add create form

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -1,22 +1,120 @@
 import useSWR from "swr";
 
 import "../styles/create-form.scss";
+import axios from "axios";
+import { useState } from "react";
 
 export default function CreateForm(props) {
+    const [itemData, setItemData] = useState({
+        img: "melee",
+        name: "",
+        price: "",
+        quantity: "",
+        attack: "",
+        accuracy: "",
+        effect: "",
+        multiplier: "",
+        info: "",
+    });
+
+    let handleChange = (e) => {
+        let target = e.target;
+        let value = target.value;
+        let name = target.name;
+
+        console.log(name + " " + value);
+        setItemData({
+            ...itemData,
+            [name]: value,
+        });
+    };
+
     let handleSubmit = (e) => {
         e.preventDefault();
 
-        console.log(e.target.name.value);
+        console.log(itemData);
+        axios
+            .post("/api/meleeweapons/create", { meleeweapon: itemData })
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+            });
     };
-
-    let handleChange = (e) => {};
 
     return (
         <section className="form-container">
             <form onSubmit={handleSubmit}>
                 <label>
-                    Person Name:
-                    <input type="text" name="name" onChange={handleChange} />
+                    Name:
+                    <input
+                        type="text"
+                        name="name"
+                        value={itemData.name}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Price:
+                    <input
+                        type="text"
+                        name="price"
+                        value={itemData.price}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Quantity:
+                    <input
+                        type="text"
+                        name="quantity"
+                        value={itemData.quantity}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Attack:
+                    <input
+                        type="text"
+                        name="attack"
+                        value={itemData.attack}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Accuracy:
+                    <input
+                        type="text"
+                        name="accuracy"
+                        value={itemData.accuracy}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Effect:
+                    <input
+                        type="text"
+                        name="effect"
+                        value={itemData.effect}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Multiplier:
+                    <input
+                        type="text"
+                        name="multiplier"
+                        value={itemData.multiplier}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Info:
+                    <input
+                        type="text"
+                        name="info"
+                        value={itemData.info}
+                        onChange={handleChange}
+                    />
                 </label>
                 <button type="submit">Add</button>
             </form>
